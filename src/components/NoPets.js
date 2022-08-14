907ecff83e900009b3e0766286af0b1b224a5224
unknown <hever11.hdgd@gmail.com>
2022-08-13 20:06:53 -0500

    x

---
+++ src/components/NoPets.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "react-bootstrap";
+import nopets from "../assets/nopets.gif";
+import "../css/app.css";
+
+export const NoPets = () => {
+  return (
+    <div className="container">
+      <Row>
+        <Col lg={5} md={6} sm={12} className="p-5 m-auto shadow-sm rounded-lg">
+          <div className=" text-center">
+            <img src={nopets} alt="img" className="img-fluid img" />
+            <h1 className="shawow-sm tex-success mt-5 p-3 text-center rounded init">
+              {" "}
+              No Pets Found
+            </h1>
+            <Container>
+              <Row>
+                <Col
+                  lg={5}
+                  md={6}
+                  sm={12}
+                  className="p-5 m-auto shadow-sm rounded-lg"
+                ></Col>
+                <div className=" text-center">
+                  <button className="btn btn-success">
+                    <Link to="/create">
+                      <b className="text-white"> Create New Pet </b>
+                    </Link>
+                  </button>
+                </div>
+              </Row>
+            </Container>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { getPets, deletePet, updatePet } from "../services";
+import { getPets, deletePet } from "../services";
 import { GrUpdate } from "react-icons/gr";
 import { AiFillDelete } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { NoPets } from "./NoPets";
 
 export function Pets() {
   const [pets, setPets] = useState([]);
@@ -26,56 +27,63 @@ export function Pets() {
   return (
     <>
       <div className="App">
-        <div className="text-center-my-3">
-          <h1 className="text-center">
-            <b> Pets List</b>
-          </h1>
-        </div>
+        {!pets.length ? (
+          <NoPets />
+        ) : (
+          <div className="container">
+            <div className="text-center-my-3">
+              <h1 className="text-center">
+                <b> Pets List</b>
+              </h1>
+            </div>
 
-        <div className="row">
-          {pets.map((pets) => {
-            return (
-              <div
-                className="card-group col-xs-12 col-sm-6 col-md-4 col-lg-3"
-                key={pets._id}
-              >
-                <div className="card my-3">
-                  <img
-                    src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    className=" img-rounded rounded float-left card-img-top img-thumbnail img-fluid"
-                    alt="..."
-                  />
-                  <h5 className="card-title text-center">
-                    <b>Name : {pets.name}</b>
-                  </h5>
-                  <div className="card-body">
-                    <p className="card-text">
-                      Age : <b>{pets.age}</b>
-                    </p>
-                    <p className="card-text">
-                      Specie : <b>{pets.specie ? pets.specie : "No Specie"}</b>
-                    </p>
+            <div className="row">
+              {pets.map((pets) => {
+                return (
+                  <div
+                    className="card-group col-xs-12 col-sm-6 col-md-4 col-lg-3"
+                    key={pets._id}
+                  >
+                    <div className="card my-3">
+                      <img
+                        src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+                        className=" img-rounded rounded float-left card-img-top img-thumbnail img-fluid"
+                        alt="..."
+                      />
+                      <h5 className="card-title text-center">
+                        <b>Name : {pets.name}</b>
+                      </h5>
+                      <div className="card-body">
+                        <p className="card-text">
+                          Age : <b>{pets.age}</b>
+                        </p>
+                        <p className="card-text">
+                          Specie :{" "}
+                          <b>{pets.specie ? pets.specie : "No Specie"}</b>
+                        </p>
+                      </div>
+                      <div className="card-footer">
+                        <Link to={`/update/${pets._id}`}>
+                          <li className="btn btn-primary">
+                            <GrUpdate /> Update
+                          </li>
+                        </Link>
+                        &nbsp;
+                        <button
+                          onClick={() => handleDelete(pets._id)}
+                          className="btn btn-danger btn-block"
+                        >
+                          <AiFillDelete />
+                          delete pet
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="card-footer">
-                    <Link to={`/update/${pets._id}`}>
-                      <li className="btn btn-primary">
-                        <GrUpdate /> Update
-                      </li>
-                    </Link>
-                    &nbsp;
-                    <button
-                      onClick={() => handleDelete(pets._id)}
-                      className="btn btn-danger btn-block"
-                    >
-                      <AiFillDelete />
-                      delete pet
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
